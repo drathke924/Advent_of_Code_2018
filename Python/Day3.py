c2d328faf8bdd2d6002ba4d62fb3e5e91ef48c90
drathke924 <derekrathke@gmail.com>
2019-05-01 07:40:02 -0700

    Day 3 Python complete

--- Python/Day3.py
+++ Python/Day3.py
@@ -2,21 +2,34 @@ with open('day3.txt') as f:
     data = f.read().splitlines()
 
 rects = []
-mins = (10000, 10000)
-maxes = (0, 0)
+grid = {}
+partone = 0
 for s in data:
     s = s[1:].split(' ')
     s.pop(1)
     s[1] = tuple(map(int, s[1][0:-1].split(',')))
     s[2] = tuple(map(int, s[2].split('x')))
     rects.append((s[1][0], s[1][0] + s[2][0] - 1, s[1][1], s[1][1] + s[2][1] - 1))
-    if rects[-1][1] > maxes[0]:
-        maxes = (rects[-1][0], maxes[1])
-    if rects[-1][3] > maxes[1]:
-        maxes = (maxes[0], rects[-1][3])
-    if rects[-1][0] < mins[0]:
-        mins = (rects[-1][0], mins[1])
-    if rects[-1][2] < mins[1]:
-        mins = (mins[0], rects[-1][2])
 
-print(rects)
+for xmin, xmax, ymin, ymax in rects:
+    for x in range(xmin, xmax + 1):
+        for y in range(ymin, ymax + 1):
+            try:
+                if grid[(x, y)] == 1:
+                    grid[(x, y)] += 1
+                    partone += 1
+            except KeyError:
+                grid[(x, y)] = 1
+
+for xmin, xmax, ymin, ymax in rects:
+    overlapped = False
+    for x in range(xmin, xmax + 1):
+        for y in range(ymin, ymax + 1):
+            if grid[(x,y)] > 1:
+                overlapped = True
+                break
+    if overlapped == False:
+        parttwo = rects.index((xmin, xmax, ymin, ymax)) + 1
+
+print("Part one: " + str(partone))
+print("Part two: " + str(parttwo))
